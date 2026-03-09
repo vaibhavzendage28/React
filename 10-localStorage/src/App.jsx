6272dc31acf9d64d2fed026268bd873138b8a0fe
vaibhavzendage28 <vaibhavzendage@gmial.com>
2026-03-09 15:24:31 +0530

    revised local storage basics

--- 10-localStorage/src/App.jsx
+++ 10-localStorage/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  localStorage.setItem('name', 'John Doe') // creates a new item in localStorage with key 'name' and value 'John Doe'
+
+  const name = localStorage.getItem('name') // retrieves the value associated with the key 'name' from localStorage and assigns it to the variable 'name'
+
+  console.log(name) // Output: John Doe
+
+  // localStorage.removeItem('name') // Removes the item with key 'name' from localStorage
+
+  const user = {
+    name: 'Jane Doe',
+    age: 30,
+    email: 'janedoe@gmail.com'
+  }
+
+  localStorage.setItem('user', JSON.stringify(user)) // Converts the user object to a JSON string and stores it in localStorage with the key 'user'
+
+  const storedUser = localStorage.getItem('user') // Retrieves the JSON string associated with the key 'user' from localStorage and assigns it to the variable 'storedUser'
+
+  const parsedUser = JSON.parse(storedUser) // Parses the JSON string back into a JavaScript object and assigns it to the variable 'parsedUser'
+
+  console.log(parsedUser) // Output: { name: 'Jane Doe', age: 30, email: '
+
+  // localStorage.clear() // Clears all items from localStorage
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>App</div>
   )
 }
 
